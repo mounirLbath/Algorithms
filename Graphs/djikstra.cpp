60d8368c498377d5151bb1d2fec91d5e54dd2b7a
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2025-01-31 17:45:24 +0100

    djikstra

---
+++ Graphs/djikstra.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Djikstra algorithm
+
+vector<pair<int, int>> graph[1001]; // adjency list with pairs (edgeEnd, weight)
+int dist[1001]; //distance to s node
+bool seen[1001];
+int INF = 10000001;
+
+int main()
+{
+    int N, E, s;
+    cin >> N >> E >> s; // number of nodes, number of edges, start node
+
+    int a,b,w;
+    for(int i = 0; i < E; ++i)
+    {
+        cin >> a >> b >> w;
+        graph[a].push_back(make_pair(b, w));
+        graph[b].push_back(make_pair(a, w));
+    }
+
+    for(int i = 1; i <= N; ++i)
+    {
+        dist[i] = INF;
+    }
+    dist[s] = 0;
+
+    priority_queue<pair<int,int>> pq; // stores pairs (-dist, node) of nodes to explore
+    pq.push(make_pair(0, s));
+
+    int curr, d;
+    while(!pq.empty())
+    {
+        tie(d, curr) = pq.top();
+        pq.pop();
+        if(seen[curr])
+            continue;
+        seen[curr] = true;
+        
+        d = -d;
+        cout << d << ' ' << curr << '\n';
+
+        for(auto x : graph[curr])
+        {
+            if(dist[x.first] > d + x.second)
+            {
+                dist[x.first] = d + x.second;
+                pq.push(make_pair(-dist[x.first], x.first));
+            }
+        }
+    }
+
+}
